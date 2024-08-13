642bf946f85c07bd4675b4ace7af7f775839b81c
yshrkume <yshrkume@gmail.com>
2024-08-13 14:32:50 +0900

    fix

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-# from flask_migrate import Migrate
 from flask_restful import Api
 import os
 from models import db
@@ -14,7 +11,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
-# migrate = Migrate(app, db)
 api = Api(app)
 
 with app.app_context():
